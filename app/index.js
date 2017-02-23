d9f2d2b4998fe63a1d9651178192d181a66d28b6
Stephanie Andrews <seandrews11@gmail.com>
2017-02-23 14:28:18 -0700

    reorganize basic structure

--- app/index.js
+++ app/index.js
@@ -1,8 +1,24 @@
-import React from 'react'
 import { render } from 'react-dom'
-import App from './components/app/app.js'
+import React, { Component } from 'react'
+import { Router, Route, Link, IndexRoute, browserHistory} from 'react-router'
+// import {Provider} from 'react-redux';
+
+import Home from './components/home/home.js'
+// import store from '../../../app/store.js'
 
 import style from './styles.scss'
 
+const router = (
+      // <Provider store={store}>
+        <Router history={browserHistory}>
+          <Route path='/' component={Home} />
+            <IndexRoute component={Home} />
+          {/* <Route path='/create-account' component={create-account} />
+          <Route path='/sign-up' component={sign-up} /> */}
+        </Router>
+      // </Provider>
+)
 
-render(<App />, document.querySelector('.application'));
+
+
+render(router, document.querySelector('.application'));
